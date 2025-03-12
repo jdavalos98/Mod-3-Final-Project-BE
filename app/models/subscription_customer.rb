e836bcf3f3625c66820972284c82b3142d66217d
Joel Davalos <jsdavalos@gmail.com>
2025-03-11 22:47:41 -0600

    Added a function to handle updating status in the model

--- app/models/subscription_customer.rb
+++ app/models/subscription_customer.rb
@@ -4,4 +4,19 @@ class SubscriptionCustomer < ApplicationRecord
 
   validates :subscription_id, :customer_id, presence: true
   validates :status, inclusion: { in: [true, false]}
+
+  def self.update_status(customer_id, subscription, subscription_customer)
+    if subscription_customer
+      new_status = !subscription_customer.status
+      subscription_customer.update!(status: new_status)
+      active_subs = subscription.subscription_customers.where(status: true).count
+      subscription.update!(user_subscribed: active_subs)
+    else
+      subscription_customer = subscription.subscription_customer.create!(
+        customer_id: customer_id,
+        status: true
+      )
+    end
+    subscription_customer
+  end
 end
