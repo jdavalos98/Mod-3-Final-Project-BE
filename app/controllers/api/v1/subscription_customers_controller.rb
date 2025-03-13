995e2030487a2b83c12b3dfcba2561ad92998775
Joel Davalos <jsdavalos@gmail.com>
2025-03-13 14:51:09 -0600

    Wrote the update action in subscription_customers controller

--- app/controllers/api/v1/subscription_customers_controller.rb
+++ app/controllers/api/v1/subscription_customers_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::SubscriptionCustomersController < ApplicationController
+
   def update
-    subscription = Subscription.find(params[:subscription_id])
-    sub_customer = subscription.subscription_customers.find_by(customer_id: params[:customer_id])
-    updated_sub_status = SubscriptionCustomer.update_status(params[:customer_id], subscription, sub_customer)
-    render json: SubscriptionSerializer.new(subscription, {
-      params: { include_customers: true, include_teas: true }
-    }), status: :ok
+    binding.pry
+    subscription = Subscription.find_by(id: params[:subscription_id])
+    customer = Customer.find_by(id: params[:customer_id])
+  
+    if subscription && customer
+      subscription_customer = SubscriptionCustomer.update_status(params[:subscription_id], params[:customer_id], params[:status])
+      render json: SubscriptionSerializer.new(subscription, {
+        params: { include_customers: true, include_teas: true }
+      }), status: :ok
+    else
+      render json: { error: 'Subscription or Customer not found' }, status: :not_found
+    end
   end
+  # def update
+  #   subscription = Subscription.find(params[:subscription_id])
+  #   sub_customer = subscription.subscription_customers.find_by(customer_id: params[:customer_id])
+  #   updated_sub_status = SubscriptionCustomer.update_status(params[:customer_id], subscription, sub_customer)
+  #   render json: SubscriptionSerializer.new(subscription, {
+  #     params: { include_customers: true, include_teas: true }
+  #   }), status: :ok
+  # end
 end
