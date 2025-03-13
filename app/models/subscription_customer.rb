dca2214668337f52b57bfe3a0c453a53cf3703b5
Joel Davalos <jsdavalos@gmail.com>
2025-03-13 14:34:41 -0600

    Refactored update_status method in model file

--- app/models/subscription_customer.rb
+++ app/models/subscription_customer.rb
@@ -5,18 +5,37 @@ class SubscriptionCustomer < ApplicationRecord
   validates :subscription_id, :customer_id, presence: true
   validates :status, inclusion: { in: [true, false] }
 
-  def self.update_status(customer_id, subscription, subscription_customer)
+  def self.update_status(subscription_id, customer_id, new_status)
+    subscription_customer = find_by(subscription_id: subscription_id, customer_id: customer_id)
+
     if subscription_customer
-      new_status = !subscription_customer.status
       subscription_customer.update!(status: new_status)
-      active_subs = subscription.subscription_customers.where(status: true).count
-      subscription.update!(customers_subscribed: active_subs)  # Changed to customers_subscribed
     else
-      subscription_customer = subscription.subscription_customers.create!(
+      subscription = Subscription.find(subscription_id)
+      subscription_customer = create!(
+        subscription_id: subscription_id,
         customer_id: customer_id,
-        status: true
+        status: new_status
       )
     end
+    subscription_customer.subscription.update(
+      customers_subscribed: subscription_customer.subscription.subscription_customer.where(status: true).count
+    )
     subscription_customer
   end
+
+  # def self.update_status(customer_id, subscription, subscription_customer)
+  #   if subscription_customer
+  #     new_status = !subscription_customer.status
+  #     subscription_customer.update!(status: new_status)
+  #     active_subs = subscription.subscription_customers.where(status: true).count
+  #     subscription.update!(customers_subscribed: active_subs)
+  #   else
+  #     subscription_customer = subscription.subscription_customers.create!(
+  #       customer_id: customer_id,
+  #       status: true
+  #     )
+  #   end
+  #   subscription_customer
+  # end
 end
